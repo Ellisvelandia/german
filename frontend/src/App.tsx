4b43f12be57f73faf47fc7ade82353d662e81216
ellis  velandia caicedo <100318892+Ellisvelandia@users.noreply.github.com>
2025-02-19 22:35:46 -0500

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,8 +11,8 @@ function App() {
           <Route path="/" element={<ScenarioSelector />} />
           <Route path="/restaurant" element={<ChatInterface scenario="restaurant" />} />
           <Route path="/shopping" element={<ChatInterface scenario="supermarket" />} />
-          <Route path="/train" element={<ChatInterface scenario="train" />} />
-          <Route path="/conversation" element={<ChatInterface scenario="conversation" />} />
+          <Route path="/train" element={<ChatInterface scenario="train station" />} />
+          <Route path="/conversation" element={<ChatInterface scenario="free conversation" />} />
         </Routes>
       </div>
     </Router>
